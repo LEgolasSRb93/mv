4c2b82ced3f989e610676b59797cdc2443e60a3d
Lazar Cakovic <lazar.cakovic@novelic.com>
2018-07-04 18:03:47 +0200

    MOD: Modified source code, commented something, and started Report writing

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def calibration(cap, count):
 
     if circles is not None:
         circles = np.uint16(np.around(circles[0]))
-        #  print(circles.shape[0])
         if circles.shape[0] == 18:
             circles = circles[circles[:,0].argsort()]
             circlesFirst = circles[:6]
@@ -31,15 +30,6 @@ def calibration(cap, count):
             circlesThird = circles[12:]
             circlesThird = circlesThird[circlesThird[:,1].argsort()]
             circles = np.vstack((circlesFirst, circlesSecond, circlesThird))
-            #  print(circles)
-            #  circles[:6] = circlesFirst
-            #  circles[6:12] = circlesSecond
-            #  circles[12:] = circlesThird
-            #  for i in circles[:]:
-                #  # draw the outer circle
-                #  cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-                #  # draw the center circle
-                #  cv2.circle(frame,(i[0],i[1]),1,(0,0,255),2)
             count = count + 1
     else:
         circles = np.zeros([18,3], dtype=int)
@@ -57,69 +47,83 @@ fgbg = cv2.createBackgroundSubtractorMOG2()
 
 count = 0
 i = 0
+mode = 1
 
-circlesAll = np.zeros([18,3], dtype=int)
-diodeState = np.zeros([18,1], dtype=int)
+if mode == 0:
+    while(True):
+        # getting one frame from camera input
+        ret, frame = cap.read()
 
-while(True):
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+else:
+    circlesAll = np.zeros([18,3], dtype=int)
+    diodeState = np.zeros([18,1], dtype=int)
 
-    ### calibration part
-    while count<10:
-        #  print(count)
-        frame, count, circlesTemp = calibration(cap, count)
-        if circlesTemp.shape[0] == 18:
-            circlesAll += circlesTemp
+    print("Calibration started...")
+    while(True):
 
-    if count == 10:
-        circlesAll = np.uint16(np.around(circlesAll/10))
-        print(circlesAll)
-        count = 100
+        ### calibration part
+        while count<10:
+            #  print(count)
+            frame, count, circlesTemp = calibration(cap, count)
+            if circlesTemp.shape[0] == 18:
+                circlesAll += circlesTemp
 
-    # getting one frame from camera input
-    ret, frame = cap.read()
+        if count == 10:
+            print("Calibration ended...")
+            circlesAll = np.uint16(np.around(circlesAll/10))
+            print(circlesAll)
+            count = 100
 
-    fgmask = fgbg.apply(frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-    output = cv2.connectedComponentsWithStats(fgmask, 4, cv2.CV_32S)
-    for i in range(output[0]):
-        if output[2][i][4] >= 8000 and output[2][i][4] <= 100000:
-            cv2.rectangle(frame, (output[2][i][0], output[2][i][1]), (output[2][i][0] + output[2][i][2], output[2][i][1] + output[2][i][3]), (0, 255, 0), 2)
+        # getting one frame from camera input
+        ret, frame = cap.read()
 
-    if output[0] == 1:
-        ### processing part
-        # make gray image
-        grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # equilize histogram
-        modifiedImg = cv2.equalizeHist(grayImg)
-        # smoth equilized image
-        smothedImg = cv2.GaussianBlur(modifiedImg, (5, 5), 1.5)
-        # get only brightest parts of image
-        ret,binImg = cv2.threshold(grayImg, 200, 255, cv2.THRESH_BINARY)
+        fgmask = fgbg.apply(frame)
+        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+        output = cv2.connectedComponentsWithStats(fgmask, 4, cv2.CV_32S)
+        for i in range(output[0]):
+            if output[2][i][4] >= 8000 and output[2][i][4] <= 100000:
+                cv2.rectangle(frame, (output[2][i][0], output[2][i][1]), (output[2][i][0] + output[2][i][2], output[2][i][1] + output[2][i][3]), (0, 255, 0), 2)
 
-        os.system('clear')
+        if output[0] == 1:
+            ### processing part
+            # make gray image
+            grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # equilize histogram
+            modifiedImg = cv2.equalizeHist(grayImg)
+            # smoth equilized image
+            smothedImg = cv2.GaussianBlur(modifiedImg, (5, 5), 1.5)
+            # get only brightest parts of image
+            ret,binImg = cv2.threshold(grayImg, 200, 255, cv2.THRESH_BINARY)
+            os.system('clear')
 
-        #  print(circlesAll)
-        # ask for
-        for i in range(0, 18):
-            # ask for every found diode is it white or black (on or off)
-            if binImg[circlesAll[i][1],circlesAll[i][0]] == 255:
-                diodeState[i] = 1
-                #  print("jedna upaljena")
-            else:
-                diodeState[i] = 0
-                #  print("jedna ugasena")
-        print(diodeState)
+            print("    ROW  |   COL   |   VALUE")
+            # ask for
+            for i in range(0, 18):
+                # ask for every found diode is it white or black (on or off)
+                if binImg[circlesAll[i][1],circlesAll[i][0]] == 255:
+                    diodeState[i] = 1
+                else:
+                    diodeState[i] = 0
+                row, col = divmod(i, 6)
+                print("   ", row+1, "   |   ", col+1, "   |  ", diodeState[i])
+            #  print(diodeState)
 
-        ### drawing positions of diodes calibration have found
-        for i in circlesAll[:]:
-            # draw the outer circle
-            #  cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center circle
-            cv2.circle(frame,(i[0],i[1]),1,(0,0,255),2)
+            ### drawing positions of diodes calibration have found
+            for i in circlesAll[:]:
+                # draw the outer circle
+                #  cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+                # draw the center circle
+                cv2.circle(frame,(i[0],i[1]),1,(0,0,255),2)
+        else:
+            os.system('clear')
+            print("Interruption detected!")
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
